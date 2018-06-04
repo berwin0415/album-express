f11e03805b5e9b9663dedd437d3651e8b9891251
berwin han <berwin0415@163.com>
2018-06-04 15:11:30 +0800

    格式化

--- app.js
+++ app.js
@@ -14,37 +14,39 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev')); 
+app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 // 配置session
 app.use(session({
-   secret: "aihfoihweoifhoiwahfoieahfio",
-   resave: false,
-   saveUninitialized: true
+  secret: "aihfoihweoifhoiwahfoieahfio",
+  resave: false,
+  saveUninitialized: true
 }));
 
 app.use('/users', usersRouter);
 // 登录拦截
 app.use(function (req, res, next) {
-	var username  = req.session.username;
-	if (username) {
-		next()
-	}else{
-        res.redirect("/users/login");
-	}
+  var username = req.session.username;
+  if (username) {
+    next()
+  } else {
+    res.redirect("/users/login");
+  }
 })
 app.use('/api', apiRouter);
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
